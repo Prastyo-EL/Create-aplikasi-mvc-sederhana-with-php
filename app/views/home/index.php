57c3b6d7aa7157733203ed41ee3861fbccda8862
Prastyo-EL <52477300+Prastyo-EL@users.noreply.github.com>
2021-10-23 00:14:37 +0700

    test vscode.dev

--- app/views/home/index.php
+++ app/views/home/index.php
@@ -1,6 +1,6 @@
 <div class="container">
 <div class="jumbotron mt-4">
-  <h1 class="display-4">Selamat datang di website saya!</h1>
+  <h1 class="display-4">Selamat datang di website saya gays!</h1>
   <p class="lead">Hallo nama saya <?= $data['nama'];?></p>
   <hr class="my-4">
   <p>It uses utility classes for typography and spacing to space content out within the larger container.</p>
